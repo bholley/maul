2a88d206cca1f8f8c96884d6f66da07e790e11a0
derosenfeld <dero@stanford.edu>
2010-10-31 21:18:00 -0700

    comment added to parsedatafiles.sh

--- data-raw/parsedatafiles.sh
+++ data-raw/parsedatafiles.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/sh
+# CURRENT SYSTEM USES UAS PARSER AND USER_AGENT.PY PARSER FROM BROWSERSCOPE
+# This generates somewhat curated data
 python csv_parse.py
 python xml_parse.py
 python merge_data.py
