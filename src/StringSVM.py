50217656cb09ddf6382337cca40b8f4397b91010
Bobby Holley <bobbyholley@gmail.com>
2010-11-15 21:47:41 -0800

    fixed some bugs in StringSVM

--- src/StringSVM.py
+++ src/StringSVM.py
@@ -95,7 +95,7 @@ class StringSVM:
     # Generate the model
     self.model = self.svmlib.svm_train(problem, params)
 
-  def predict(self, string):
+  def predict(self, val):
 
     # Validate
     if not hasattr(self, 'model'):
@@ -104,7 +104,14 @@ class StringSVM:
     # Make a prediction
     query = svm_data()
     query.v = None
-    query.s = string
+    if (self.tokenized):
+      query.t = (c_uint * (len(val) + 1))()
+      query.t[0] = len(val)
+      for j,num in enumerate(val):
+        query.t[j+1] = num
+    else:
+      query.s = val
+    print query
     prediction = self.svmlib.svm_predict_p(self.model, pointer(query))
 
     return self.reverseLabelMap[int(prediction)]
@@ -202,7 +209,10 @@ class svm_problem(Structure):
     for i, val in enumerate(values):
       self.x[i].v = None
       if (stringSVM.tokenized):
-        self.x[i].t = val
+        self.x[i].t = (c_uint * (len(val) + 1))()
+        self.x[i].t[0] = len(val) # First element of a token string is the length
+        for j, num in enumerate(val):
+          self.x[i].t[j+1] = num
       else:
         self.x[i].s = val
 
