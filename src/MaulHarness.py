69a21cc484f837a8ccf25e906fb516ea4b0fbc75
derosenfeld <dero@stanford.edu>
2010-12-06 00:18:26 -0800

    left Maulharness in usable state

--- src/MaulHarness.py
+++ src/MaulHarness.py
@@ -20,32 +20,32 @@ random.seed(18283835)
 #dataset.crossValidate('Type', 0.8, 1.0, "WHERE (Type = 'Browser' OR Type = 'Robot')")
 #dataset.crossValidate('Family', 0.8, 1.0, "WHERE (Type = 'Browser' AND Family IS NOT NULL)")
 #dataset.crossValidate('OS',0.8,1.0,"WHERE (Type = 'Browser' AND OS IS NOT NULL)")
-#dataset.crossValidate('Type', 0.8, 1.0, "WHERE (Type = 'Browser' OR Type = 'Robot' OR Type = 'Mobile Browser')")
+dataset.crossValidate('Type', 0.8, 1.0, "WHERE (Type = 'Browser' OR Type = 'Robot' OR Type = 'Mobile Browser')")
 
 
 
 
 # playing with hard to classify strings
-dataset = MaulDataset('../data/maul.db.excl', params) # load DB which excluded the hard to classify  strings
+#dataset = MaulDataset('../data/maul.db.excl', params) # load DB which excluded the hard to classify  strings
 # train on all of the dataset
-dataset.crossValidate('Type', 1.0, 1.0, "WHERE (Type = 'Browser' OR Type = 'Robot' OR Type = 'Mobile Browser')")
+#dataset.crossValidate('Type', 1.0, 1.0, "WHERE (Type = 'Browser' OR Type = 'Robot' OR Type = 'Mobile Browser')")
 
 
 # load nasty strings
-testset = MaulDataset('../data/nastydb',params)
-testset.loadSamples('Type',"WHERE (Type = 'Browser' OR Type = 'Robot' OR Type = 'Mobile Browser')")
+#testset = MaulDataset('../data/nastydb',params)
+#testset.loadSamples('Type',"WHERE (Type = 'Browser' OR Type = 'Robot' OR Type = 'Mobile Browser')")
 
 # format into same way prediction is run inside
-sampleList = list()
-for key in testset.samples.keys():
-    sampleList.extend(testset.samples[key])
+#sampleList = list()
+#for key in testset.samples.keys():
+#    sampleList.extend(testset.samples[key])
 
 # predict and output results
-for sample in sampleList:
-    prepared = testset.prepareSample(sample)
-    prediction = dataset.problem.decide(prepared[1])
-    print 'pred: ', prediction, ' actual: ', sample['label']
-    print sample['uaString']
+#for sample in sampleList:
+#    prepared = testset.prepareSample(sample)
+#    prediction = dataset.problem.decide(prepared[1])
+#    print 'pred: ', prediction, ' actual: ', sample['label']
+#    print sample['uaString']
 
 
 
